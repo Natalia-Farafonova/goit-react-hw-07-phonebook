eef6643adcbf568d073a29980d79ccfd28e111f2
Наталия Фарафонова <ferymfon1983@gmail.com>
2022-01-25 11:06:34 +0200

    Сделала рефакторинг кода

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,18 +1,43 @@
 import { configureStore } from '@reduxjs/toolkit';
-import contacts from '../slices/contacts';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { contactsApi } from '../slices/contacts';
 import filter from '../slices/filter';
 
-const rootReducer = {
-  contacts,
-  filter,
-};
-
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    filter: filter,
+    [contactsApi.reducerPath]: contactsApi.reducer,
+  },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware(),
+    contactsApi.middleware,
+  ],
   devTools: process.env.NODE_ENV !== 'production',
 });
 
+setupListeners(store.dispatch);
+
 export default store;
+
+// ================================================================================
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import contacts  from '../slices/contacts';
+// import filter from '../slices/filter';
+
+// const rootReducer = {
+//   contacts,
+//   filter,
+// };
+
+// const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: process.env.NODE_ENV !== 'production',
+// });
+
+// export default store;
+
+// ===========================================================================================
 
 // import { createStore } from 'redux';
 // import { devToolsEnhancer } from 'redux-devtools-extension';
